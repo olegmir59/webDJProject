3d69af28b67ae68bde717196d9e5fb87b2382067
olegmir59 <udom1@ya.ru>
2025-09-05 02:19:07 +0700

    Add and modify files

--- ZeroCoder/main/urls.py
+++ ZeroCoder/main/urls.py
@@ -2,7 +2,10 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index),
-    path('data', views.data),
-    path('test', views.test)
+    path('', views.index, name='index'),
+    path('about/', views.about, name='about'),
+    path('contact/', views.contact, name='contact'),
+    path('services/', views.services, name='services'),
+    path('data', views.data, name='data'),
+    path('test', views.test, name='test')
 ]
--- ZeroCoder/main/views.py
+++ ZeroCoder/main/views.py
@@ -2,10 +2,20 @@ from django.shortcuts import render
 
 # Create your views here.
 from django.http import HttpResponse
+from django.shortcuts import render
 
 
 def index(request):
-    return HttpResponse("<h1>Это мой первый проект на Django</h1>")
+    return render(request, 'main/index.html')
+
+def about(request):
+    return render(request, 'main/about.html')
+
+def contact(request):
+    return render(request, 'main/contact.html')
+
+def services(request):
+    return render(request, 'main/services.html')
 
 
 def data(request):
